45dce8a1b92217b3488039ba6c0f0eb927e6bd16
auralic <359209162@qq.com>
2015-05-17 08:51:37 +0800

    Initial Commit

---
+++ git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git
+//
+//  Created by auralic on 15/5/17.
+//  Copyright (c) 2015年 auralic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
